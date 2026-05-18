32e4f162ed27eabab53a8181f25a61c65033257d
Nina Li <nina.lining@gmail.com>
2026-05-18 22:14:59 +0200

    Configure SQLite and add Book entity with migration

---
+++ src/Repository/BookRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Book;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Book>
+ */
+class BookRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Book::class);
+    }
+
+    public function findByIsbn(string $isbn): ?Book
+    {
+        return $this->findOneBy(['isbn' => $isbn]);
+    }
+}
